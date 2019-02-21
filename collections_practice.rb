22d2a7291918ebf277ae9eb324684c6196e40d67
Cody Yantis <cyantis@gmail.com>
2019-02-21 01:49:27 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,48 @@
+require 'pry'
 
+def sort_array_asc(array)
+  array.sort
+end
+
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort {|x, y| x.length <=> y.length}
+end
+
+def swap_elements(array)
+  second = array[1]
+  third = array[2]
+  array[1] = third
+  array[2] = second
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  new_arr = []
+  array.each do |string|
+    string[2] = "$"
+    new_arr << string
+  end
+  new_arr
+end
+
+def find_a(array)
+  a_arr = []
+  array.each {|a| a_arr << a if a.start_with?("a")}
+  a_arr
+end
+
+def sum_array(array)
+  array.inject {|x, y| x + y}
+end
+
+def add_s(array)
+  array.each_with_index.collect{|e, i| i == 1 ? e : e += "s"}
+end
